6ec54f6b4740086400bdf5a8db3e24163f826f60
Alexis Lopez Zubieta <azubieta90@gmail.com>
2019-04-27 04:06:30 -0500

    Properly configure package exports

--- conanfile.py
+++ conanfile.py
@@ -10,15 +10,17 @@ class XdgutilscxxConan(ConanFile):
     description = "Implementation of the FreeDesktop specifications to be used in c++ projects"
     topics = ("FreeDesktop", "xdg-utils", "c++")
     settings = "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
-    generators = ["cmake"]
+    options = {"fPIC": [True, False]}
+    default_options = {"fPIC": True}
+    build_requires = "cmake_installer/3.13.0@conan/stable"
+    generators = ["cmake_paths"]
 
     def source(self):
         self.run("git clone https://github.com/azubieta/xdg-utils-cxx.git")
 
     def build(self):
         cmake = CMake(self)
+        cmake.definitions["CMAKE_PROJECT_XdgUtils_INCLUDE"] = self.build_folder + "/conan_paths.cmake"
         cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = self.options.fPIC
         cmake.configure(source_folder="xdg-utils-cxx")
         cmake.build()
@@ -26,3 +28,5 @@ class XdgutilscxxConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = ["XdgUtilsBaseDir", "XdgUtilsDesktopEntry"]
+        self.cpp_info.builddirs = ["lib/cmake/XdgUtils/"]
+        self.cpp_info.includedirs = ["include/"]
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -5,17 +5,13 @@ from conans import ConanFile, CMake, tools
 
 class XdgutilscxxTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    options = {"fPIC": [True, False]}
-    default_options = {"fPIC": True}
-    generators = ["cmake", "cmake_paths"]
+    generators = ["cmake_paths"]
 
     def build(self):
         cmake = CMake(self)
+        cmake.definitions["CMAKE_PROJECT_PackageTest_INCLUDE"] = self.build_folder + "/conan_paths.cmake"
         cmake.configure()
         cmake.build()
-
-    def imports(self):
-        self.copy('*.so*', dst='bin', src='lib')
 
     def test(self):
         if not tools.cross_building(self.settings):
